b2954c3307ed4aa60909fffb2bdf77726261ef59
Kristofer1990 <93189609+Kristofer1990@users.noreply.github.com>
2021-10-26 12:20:40 +0300

    Ex1

---
+++ EX1.py
@@ -0,0 +1,9 @@
+while 1:
+    x = int(input("ввод\n"))
+    if x == -1 : break
+    if x% 2 == 0:
+        print(str(x) + " четное число")
+    else :
+        print(str(x) + "- нечетное число")
+
+   
